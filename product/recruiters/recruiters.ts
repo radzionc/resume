2f7a0d1d0ee4aae5b235563f5d6f5e8d3426cc50
Rodion Chachura <geekrodion@gmail.com>
2025-01-11 15:18:01 +0400

    Add recruiters module with initial data structure and package configuration

---
+++ product/recruiters/recruiters.ts
@@ -0,0 +1,61 @@
+type Recruiter = {
+  name: string
+  email?: string
+  telegram?: string
+  linkedin?: string
+}
+
+export const recruiters: Recruiter[] = [
+  {
+    name: 'Will',
+    telegram: '@willthetrill',
+  },
+  {
+    telegram: '@Chrissu_kai',
+    name: 'Christina',
+  },
+  {
+    telegram: '@elonslostson',
+    name: 'Alex',
+  },
+  {
+    telegram: '@EuanWilson',
+    name: 'Euan',
+  },
+  {
+    telegram: '@Mike_Odiin',
+    name: 'Mike',
+  },
+  {
+    telegram: '@AlexHypeTalent',
+    name: 'Alex',
+  },
+  {
+    telegram: '@samferber',
+    name: 'Sam',
+  },
+  {
+    telegram: '@fincooney',
+    name: 'Fin',
+  },
+  {
+    telegram: '@JamesRecruiter',
+    name: 'James',
+  },
+  {
+    telegram: '@IsabellaMessore',
+    name: 'Isabella',
+  },
+  {
+    telegram: '@JakeKellyOdiin',
+    name: 'Jake',
+  },
+  {
+    email: 'angus@calyptus.co',
+    name: 'Angus',
+  },
+  {
+    email: 'freddie@functionalworks.com',
+    name: 'Freddie',
+  },
+]

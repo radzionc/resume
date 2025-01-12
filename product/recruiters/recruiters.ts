32d18b3ba5ca2002cb6525c0c19ca59b7d178241
Rodion Chachura <geekrodion@gmail.com>
2025-01-12 08:53:10 +0400

    Add multiple recruiters to the recruiters module with LinkedIn profiles for enhanced networking opportunities

--- product/recruiters/recruiters.ts
+++ product/recruiters/recruiters.ts
@@ -58,4 +58,81 @@ export const recruiters: Recruiter[] = [
     email: 'freddie@functionalworks.com',
     name: 'Freddie',
   },
+  {
+    linkedin: 'https://www.linkedin.com/in/aaron-hayhurst/',
+    name: 'Aaron',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/amber-donlon-0555a5315/',
+    name: 'Amber',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/tomas-sharpe/',
+    name: 'Tomas',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/web3domgolab/',
+    name: 'Dominik',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/rodairos/',
+    name: 'Richard',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/alex-casali-3a625a204/',
+    name: 'Alex',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/colintaylor3/',
+    name: 'Colin',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/jsreact/',
+    name: 'Ethan',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/aaron-harrison-b7644b127/',
+    name: 'Aaron',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/rory-stevens/',
+    name: 'Rory',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/alexander-g-ab2a69201/',
+    name: 'Alexander',
+  },
+  {
+    linkedin:
+      'https://www.linkedin.com/in/tom-exelby-%E2%82%BF%F0%9F%93%88-089579172/',
+    name: 'Tom',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/jglensmark/',
+    name: 'Joshua',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/jack-klein-5a3bb271/',
+    name: 'Jack',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/colin-lenormand/',
+    name: 'Colin',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/alex-andries-646602232/',
+    name: 'Alex',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/callum-crombie/',
+    name: 'Callum',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/reese-hong/',
+    name: 'Reese',
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/jake-plaskow-b3a782243/',
+    name: 'Jake',
+  },
 ]
